6927b4d50ec3200d4dc1eae86d44f1e3c8a352d5
Stephen Searles <stephen.searles@gmail.com>
2013-06-01 15:29:10 -0700

    go fmt

--- goQuotedPrintable.go
+++ goQuotedPrintable.go
@@ -3,57 +3,56 @@
 package quotedPrintable
 
 import (
-  "strings"
-  "encoding/hex"
+	"encoding/hex"
+	"strings"
 )
 
 func NewEncoder() QuotedPrintableEncoder {
-  return QuotedPrintableEncoder{}
+	return QuotedPrintableEncoder{}
 }
 
-type QuotedPrintableEncoder struct {}
+type QuotedPrintableEncoder struct{}
 
 func (encoder QuotedPrintableEncoder) TransferEncodingType() string {
-  return "quoted-printable"
+	return "quoted-printable"
 }
 
 func (encoder QuotedPrintableEncoder) Encode(src []byte) []byte {
-  return Encode(src)
+	return Encode(src)
 }
 
 // Encodes bytes to quoted printable bytes. The resulting slice may
 // be longer than the input slice.
 func Encode(src []byte) []byte {
-  var dstBuilder []byte
-  if len(src) == 0 {
-    return dstBuilder
-  }
+	var dstBuilder []byte
+	if len(src) == 0 {
+		return dstBuilder
+	}
 
-  lineCounter := 0
-  lineMax := 76
-  for _, b := range src {
-    encoded := quotedPrintableEncodeByte(b)
-    lineCounter += len(encoded)
-    if lineCounter > lineMax {
-      dstBuilder = append(dstBuilder, []byte("=\r\n")...)
-      lineCounter = len(encoded)
-    }
-    dstBuilder = append(dstBuilder, encoded...)
-  }
+	lineCounter := 0
+	lineMax := 76
+	for _, b := range src {
+		encoded := quotedPrintableEncodeByte(b)
+		lineCounter += len(encoded)
+		if lineCounter > lineMax {
+			dstBuilder = append(dstBuilder, []byte("=\r\n")...)
+			lineCounter = len(encoded)
+		}
+		dstBuilder = append(dstBuilder, encoded...)
+	}
 
-  return dstBuilder
+	return dstBuilder
 }
 
 // Encodes bytes to a quoted printable string.
 func EncodeToString(src []byte) string {
-  return string(Encode(src))
+	return string(Encode(src))
 }
 
 func quotedPrintableEncodeByte(b byte) []byte {
-  if b == 9 || b == 32 || (b >= 33 && b <= 60) || (b >= 62 && b <= 126) {
-    return []byte{b}
-  }
-  hex := "=" + strings.ToUpper(hex.EncodeToString([]byte{b}))
-  return []byte(hex)
+	if b == 9 || b == 32 || (b >= 33 && b <= 60) || (b >= 62 && b <= 126) {
+		return []byte{b}
+	}
+	hex := "=" + strings.ToUpper(hex.EncodeToString([]byte{b}))
+	return []byte(hex)
 }
-
